dfde2dd3b4c13e89d0623a491f970a37cb97ea81
yuxin-dell-xps <yuxin_liu_1998@outlook.com>
2025-11-19 11:09:06 +0800

    flink word count

--- Flink_wordcount/src/main/java/org/example/WindowWordCount.java
+++ Flink_wordcount/src/main/java/org/example/WindowWordCount.java
@@ -5,8 +5,10 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import java.time.Duration;
 
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
 import org.apache.flink.util.Collector;
 
 public class WindowWordCount {
@@ -14,6 +16,20 @@ public class WindowWordCount {
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        //test
+//        DataStream<Tuple2<String, Integer>> dataStream = env
+//                .fromElements(
+//                        "hello world",
+//                        "flink streaming",
+//                        "hello flink"
+//                )
+//                .flatMap(new Splitter())
+//                .keyBy(value -> value.f0)
+//                .window(GlobalWindows.create())
+//                .trigger(CountTrigger.of(1))
+//                .sum(1);
+
 
         DataStream<Tuple2<String, Integer>> dataStream = env
                 .socketTextStream("localhost", 9999)
@@ -25,12 +41,15 @@ public class WindowWordCount {
         dataStream.print();
 
         env.execute("Window WordCount");
+
     }
 
     public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
         @Override
         public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            System.out.println("Received sentence: " + sentence);
             for (String word: sentence.split(" ")) {
+                System.out.println("Emitting: " + word);
                 out.collect(new Tuple2<String, Integer>(word, 1));
             }
         }
